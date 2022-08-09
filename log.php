bbedae040c2c2a717a1fb58c8f5e9d7436e9e0e1
Abhijeet kumar <80936610+Abhijeetbyte@users.noreply.github.com>
2022-08-10 04:06:38 +0530

    Add files via upload

---
+++ log.php
@@ -0,0 +1,38 @@
+<?php
+
+
+//varible setting
+ $name= $_REQUEST['name'];
+ $pass = $_REQUEST['pass'];
+ 
+ $username = "admin" ;
+ $password = "admin123";
+
+
+// check input fields
+if ( empty($name)|| empty($pass))
+{
+echo"<script type='text/javascript'>alert('Please fill all correct');
+    window.history.go(-1);
+    </script>";
+}
+
+// check credentials
+if ( $name == $username && $pass == $password)
+{
+echo"<script type='text/javascript'>alert('Welcome !');
+    window.history.go(-1);
+    </script>";
+}
+else 
+{ 
+
+    echo"<script type='text/javascript'>alert('Login failed. Invalid username or password');
+    window.history.go(-1);
+    </script>";
+}
+
+
+
+
+?>
